e90b11e4599277b97df78456e3206989e2ad7ea7
Tiago Pitombeira <tiagopitombeira1984@gmail.com>
2026-02-06 01:13:18 -0300

    Meu primeiro flask

---
+++ views.py
@@ -0,0 +1,12 @@
+from main import app
+from flask import render_template
+
+
+# Rotas
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+@app.route("/blog")
+def blog():
+    return "Meu blog"
